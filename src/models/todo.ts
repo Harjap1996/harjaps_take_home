9760938824e008a857c530b65b9b14a47270f18f
harjap1996 <harjap1996nov@gmail.com>
2025-06-16 13:55:27 -0700

    refactor(todo): add priority field to Todo interface

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,6 +1,8 @@
+export type Priority = "low" | "medium" | "high";
 export interface Todo {
     id: string;
     title: string;
     editing: boolean;
     completed: boolean;
+    priority: Priority;
 }
